0e2e371931d6cd4039d1ad02ed863399d313f14f
Navid EMAD <contact@navidemad.net>
2016-07-15 16:03:11 -0700

    Main User Interface and Edit README

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 <img src="/app/src/main/res/mipmap-xxhdpi/ic_launcher.png" align="left" />
 # Barcode Bag
-> Barcode Bag is a wallet application for saving all of your barcodes instead of having it in your wallet everytime.
-
-## Features
-- You can add any barcode that you want by scanning it.
-- See your most used barcodes at a glance.
+> Wallet Android App: save your barcodes into your phone instead of your wallet.
--- app/src/main/java/net/navidemad/barcodebag/BagActivity.java
+++ app/src/main/java/net/navidemad/barcodebag/BagActivity.java
@@ -3,12 +3,12 @@ package net.navidemad.barcodebag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BagActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bag);
     }
 
 }
